2165e22b73d6f3a979fa1b42a3046540eeea0b03
Daniel <dani.ag9@gmail.com>
2018-11-26 19:00:37 +0000

    Better tabbing on Client

--- Ex3/CircuitRouter-AdvShell/CircuitRouter-Client.c
+++ Ex3/CircuitRouter-AdvShell/CircuitRouter-Client.c
@@ -16,8 +16,8 @@ char path[BUFFER_SIZE];
 void apanhaINT(int sig){
 	close(fserv);
 	close(fcli);
-    unlink(path);
-    exit(0);
+	unlink(path);
+	exit(0);
 }
 
 void leComando(char* ptr, int size){
@@ -32,35 +32,35 @@ void leComando(char* ptr, int size){
 }
 
 int main(int argc, char** argv){
-    pid_t pid;
-    char buffer[BUFFER_SIZE], buffer_aux[BUFFER_SIZE], pidNumber[24], *extention = ".pipe";
+	pid_t pid;
+	char buffer[BUFFER_SIZE], buffer_aux[BUFFER_SIZE], pidNumber[24], *extention = ".pipe";
 	strcpy(path, CLI);
-    pid = getpid();
-    sprintf(pidNumber, "%d", pid);
-    strcat(path, pidNumber);
-    strcat(path, extention);
-    
-    signal(SIGINT, apanhaINT);
-    signal(SIGPIPE, apanhaINT);
+	pid = getpid();
+	sprintf(pidNumber, "%d", pid);
+	strcat(path, pidNumber);
+	strcat(path, extention);
+	
+	signal(SIGINT, apanhaINT);
+	signal(SIGPIPE, apanhaINT);
 
-    if (argc!=2){
-        printf("Falta o argumento da pipe do servidor\n");
-        exit(-1);
-    }
+	if (argc!=2){
+		printf("Falta o argumento da pipe do servidor\n");
+		exit(-1);
+	}
 
-    if ( (fserv = open(argv[1], O_WRONLY))<0){
-        printf("Erro ao abrir pipe\n");
-        exit(-1);
-    }
-    
-    unlink(path);
-    
-    if (mkfifo(path, 0777) < 0){
-        printf("Erro ao iniciar PIPE do cliente1\n");
-        exit(-1);        
-    }
+	if ( (fserv = open(argv[1], O_WRONLY))<0){
+		printf("Erro ao abrir pipe\n");
+		exit(-1);
+	}
+	
+	unlink(path);
+	
+	if (mkfifo(path, 0777) < 0){
+		printf("Erro ao iniciar PIPE do cliente1\n");
+		exit(-1);		 
+	}
 
-    while (1){
+	while (1){
 		msg_protocol msg;
 
 		buffer_aux[0] = '\0';
@@ -75,10 +75,10 @@ int main(int argc, char** argv){
 		if ((read(fcli, buffer_aux, BUFFER_SIZE))<0) exit(EXIT_FAILURE);
 		write(1, buffer_aux, strlen(buffer_aux));
 		close(fcli);
-    }
+	}
 
-    close(fserv);
-    close(fcli);
-    unlink(path);
-    exit(0);
+	close(fserv);
+	close(fcli);
+	unlink(path);
+	exit(0);
 }
